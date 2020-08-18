66ffd86047524c1b44e87d1ce873ea99025b41d2
Novak <v.novak@dl007165.dhag.rd.local>
2020-08-18 15:44:45 +0200

    Add date input, display event

--- src/countdownTimer/components/Input.js
+++ src/countdownTimer/components/Input.js
@@ -2,14 +2,29 @@ import styled from '@emotion/styled';
 
 export const InputsBox = styled.div`
   display: flex;
-  justify-items: center;
-  margin-bottom: 20px;
-  margin: 100px;
+  justify-content: center;
+  padding: 50px 0 20px;
 `;
+
 export const TextInput = styled.input`
   outline: none;
   padding: 10px;
   width: 100%;
   font-size: 1rem;
   border: none;
+  border-radius: 7px 0 0 7px;
+`;
+
+export const DateInput = styled.input`
+  border: none;
+  outline: none;
+  padding: 0 5px 0 5px;
+  border-radius: 0 7px 7px 0;
+  font-size: 1rem;
+  width: 40%;
+`;
+
+export const Div = styled.div`
+  display: flex;
+  min-width: 700px;
 `;
--- src/countdownTimer/pages/CountdownTimer.js
+++ src/countdownTimer/pages/CountdownTimer.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Main } from '../components/Main';
 import DateCountdown from 'react-date-countdown-timer';
 import { Button } from '../components/Button';
-import { InputsBox, TextInput } from '../components/Input';
+import { InputsBox, TextInput, DateInput, Div } from '../components/Input';
 import { TimerWrapper } from '../components/TimerWrapper';
 
 export default function CountdownTimer() {
+  const date = 'December 31, 2020';
+  const [event, setEvent] = useState('');
+  const [eventDate, setEventDate] = useState(date);
+  const clickOnStartButton = () => {
+    setEventDate();
+  };
+
   return (
     <Main>
       <h2>Countdown Timer</h2>
       <InputsBox>
-        <TextInput placeholder={'Write down event name'} type="text" />
-        <Button>Start</Button>
+        <Div>
+          <TextInput
+            placeholder={'Write down event name'}
+            type="text"
+            value={event}
+            onChange={(event) => setEvent(event.target.value)}
+          />
+          <DateInput type="date" />
+          <Button onClick={clickOnStartButton}>Start</Button>
+        </Div>
       </InputsBox>
+
       <TimerWrapper>
-        <p>Event Name</p>
-        <DateCountdown />
+        <p>{event}</p> {/* eventName??? */}
+        <DateCountdown dateTo={eventDate} />
       </TimerWrapper>
     </Main>
   );
