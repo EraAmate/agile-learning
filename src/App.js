ac83dbe1327c4a3743f85cd20e4733f21e940012
Novak <v.novak@dl006916.dhag.rd.local>
2020-09-04 11:27:39 +0200

    Create menu component and implement it to the app

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NotesApp from './notes app/pages/NotesApp';
 import CountdownTimer from './countdown timer/pages/CountdownTimer';
+import Menu from './menu/pages/Menu';
 
 function App() {
   return (
     <Router>
       <Switch>
+        <Route path="/menu">
+          <Menu />
+        </Route>
         <Route path="/timer">
           <CountdownTimer />
         </Route>
         <Route path="/notes">
           <NotesApp />
+        </Route>
+        <Route path="/">
+          <Menu />
         </Route>
       </Switch>
     </Router>
---
+++ src/menu/pages/Menu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Main } from '../../common/Main';
+
+export default function Menu() {
+  return (
+    <Main>
+      <h3>Chooose your topic</h3>
+      <button>Countdown Timer</button>
+      <button>Notes App</button>
+    </Main>
+  );
+}
