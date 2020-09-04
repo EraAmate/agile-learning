f3657f051d5b6d4aa7fae74e2d3e3bee588b5e16
Novak <v.novak@dl006916.dhag.rd.local>
2020-09-04 11:13:06 +0200

    Get react router dom and split the tasks

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import CountdownTimer from './countdownTimer/pages/CountdownTimer';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import NotesApp from './notes app/pages/NotesApp';
+import CountdownTimer from './countdown timer/pages/CountdownTimer';
 
 function App() {
-  return <CountdownTimer />;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/timer">
+          <CountdownTimer />
+        </Route>
+        <Route path="/notes">
+          <NotesApp />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
