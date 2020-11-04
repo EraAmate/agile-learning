60b0dbdbd0d6d31c382689c1e27c89effaed41ba
Novak <v.novak@dm000099.fritz.box>
2020-11-04 14:47:09 +0100

    Add theme provider, create default theme

--- src/App.js
+++ src/App.js
@@ -3,26 +3,29 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NotesApp from './notes app/pages/NotesApp';
 import CountdownTimer from './countdown timer/pages/CountdownTimer';
 import Menu from './menu/pages/Menu';
+import { ThemeProvider } from 'emotion-theming';
+import dark from './themes/dark';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/menu">
-          <Menu />
-        </Route>
-        <Route path="/timer">
-          <CountdownTimer />
-        </Route>
-        <Route path="/notes">
-          <NotesApp />
-        </Route>
-        <Route path="/">
-          <Menu />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={dark}>
+      <Router>
+        <Switch>
+          <Route path="/menu">
+            <Menu />
+          </Route>
+          <Route path="/timer">
+            <CountdownTimer />
+          </Route>
+          <Route path="/notes">
+            <NotesApp />
+          </Route>
+          <Route path="/">
+            <Menu />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
-
 export default App;
---
+++ src/themes/dark.js
@@ -0,0 +1,11 @@
+const dark = {
+  colors: {
+    primary: '#FFFFFF', //white
+    secondary: '#2D2D2D', //dark
+    notePrimary: '#FFFF00', //yellow
+    btnDelete: '#FF0000', //red
+    btnSecondary: '#2196F3', // blue
+    dividingLine: '#DDDDDD' // grey
+  }
+};
+export default dark;
