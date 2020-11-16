ef24ffee696b7452c0825774041a1ef1a7c03e7f
Novak <v.novak@dl006801.dhag.rd.local>
2020-11-16 13:57:01 +0100

    Fix | search notes

--- src/notes app/pages/NotesApp.js
+++ src/notes app/pages/NotesApp.js
@@ -41,13 +41,16 @@ export default function NotesApp() {
   }
 
   useEffect(() => {
-    let previewNotes = notes;
-    let result = notes.filter((note) => note.title.toLowerCase().includes(searchWord.toLowerCase(), 0));
-    setNotes(result);
+    async function showAllNotes() {
+      let result = notes.filter((note) => note.title.toLowerCase().includes(searchWord.toLowerCase(), 0));
+      setNotes(result);
 
-    if (!searchWord) {
-      setNotes(previewNotes);
+      if (!searchWord) {
+        const notes = await getAllNotes();
+        setNotes(notes);
+      }
     }
+    showAllNotes();
   }, [searchWord]);
 
   const _showNote = () => {
